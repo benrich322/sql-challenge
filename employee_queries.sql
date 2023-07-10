d90509a48e1bda898f1a48279026a96b2872eeed
Ben Richardson <benrichardson@bens-air.lan>
2023-07-09 22:29:36 -0500

    finished #5

--- employee_queries.sql
+++ employee_queries.sql
@@ -52,5 +52,16 @@ left join departments
 left join employees
 	on dept_emp.emp_no = employees.emp_no
 
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+select 
+	first_name
+	, last_name
+	, sex
+	
+from employees
+where
+	first_name = 'Hercules'
+	and last_name ILIKE 'b%'
 
 
