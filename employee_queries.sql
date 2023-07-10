087f1bbdd0345463ae00208baa5ad72052b196a3
Ben Richardson <benrichardson@bens-air.lan>
2023-07-09 22:11:25 -0500

    finished #2

--- employee_queries.sql
+++ employee_queries.sql
@@ -13,4 +13,16 @@ left join salaries
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 
+select 
+	 first_name
+	, last_name
+	, hire_date
+	
+from employees
+where 
+	extract(year from hire_date) = 1986
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+
 
