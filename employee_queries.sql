432330b3e8fb7c0e00b12b84738efdbcff52b451
Ben Richardson <benrichardson@bens-air.lan>
2023-07-09 22:39:44 -0500

    fixed formatting

--- employee_queries.sql
+++ employee_queries.sql
@@ -14,7 +14,7 @@ left join salaries
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
 
 select 
-	 first_name
+	first_name
 	, last_name
 	, hire_date
 	
@@ -25,8 +25,8 @@ where
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
 select 
-	 dept_manager.dept_no
-	 , dept_name
+	dept_manager.dept_no
+	, dept_name
 	, dept_manager.emp_no
 	, last_name
 	, first_name
